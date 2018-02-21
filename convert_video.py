1af6b16420a030b186ec84de3a1e7d8bb3afd63b
Jonathan LORIETTE <jloriett@FRC0LM00867.local>
2018-02-21 12:05:12 +0100

    use folder instead of filename

--- convert_video.py
+++ convert_video.py
@@ -2,33 +2,40 @@
 # Based on Tobi Lehman script https://tobilehman.com/blog/2013/01/20/extract-array-of-frames-from-mp4-using-python-opencv-bindings/
 import sys
 import argparse
+from os import listdir
+from os.path import isfile, join
 
 import cv2
-print(cv2.__version__)
 
-def extractImages(pathIn, pathOut,resolution=0):
-    vidcap = cv2.VideoCapture(pathIn)
-    success,image = vidcap.read()
-    count = 0
-    success = True
-    while success:
-      vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*resolution))
-      success,image = vidcap.read()
-      print ('Read a new frame: ', success)
-      cv2.imwrite( pathOut + "\\frame%d.jpg" % count, image) # add picture type as option in next version
-      count += 1
+def extractImages(folderIn, pathOut,resolution=0):
+    videofiles = [f for f in listdir(folderIn) if isfile(join(folderIn, f))]
+    count_label = 0
+    for v_file in videofiles:
+        print("Start extracting frames from {0}{1}".format(folderIn,v_file))
+        vidcap = cv2.VideoCapture(join(folderIn,v_file))
+        success,image = vidcap.read()
+        success = True
+        count = 0
+        while success:
+          vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*resolution))
+          success,image = vidcap.read()
+          print ('Read a new frame: ', success)
+          cv2.imwrite( pathOut + "\picture%d.jpg" % count_label, image) # add picture type as option in next version
+          count_label += 1
+          count +=1
 
 if __name__=="__main__":
     a = argparse.ArgumentParser()
-    a.add_argument("--pathIn", help="path to video")
+    a.add_argument("--folderIn", help="directory to video(s)")
     a.add_argument("--pathOut", help="path to images")
     a.add_argument("--ms_resolution",type=int, help="get a frame every x milliseconds. By default generate jpg for every frames")
     args = a.parse_args()
 
     if args.ms_resolution:
         print("Will genererate a frame every {0} milliseconds".format(args.ms_resolution))
-    print(args)
+
+
     if (args.ms_resolution==None):
-        extractImages(args.pathIn, args.pathOut)
+        extractImages(args.folderIn, args.pathOut)
     else:
-        extractImages(args.pathIn, args.pathOut, args.ms_resolution)
+        extractImages(args.folderIn, args.pathOut, args.ms_resolution)
